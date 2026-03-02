519856c60b050f1983e78a5c0e00351080e9b5c4
Emilia <afra57@onet.pl>
2026-03-02 13:17:32 +0100

    szkice o marce i zwroty i reklamacje

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -2,21 +2,31 @@ import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import PaymentSuccess from "./pages/PaymentSuccess";
 import PaymentCancel from "./pages/PaymentCancel";
+import AboutUs from "./pages/AboutUs";
+import Complaints from "./pages/Complaints";
 
 const routes = [
-	{
-		path: "/",
-		element: <Home />,
-	},
-	{
-		path: "/payment/success",
-		element: <PaymentSuccess />,
-	},
-	{
-		path: "/payment/cancel",
-		element: <PaymentCancel />,
-	},
-	{ path: "*", element: <NotFound /> },
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/o-marce",
+    element: <AboutUs />,
+  },
+  {
+    path: "/zwroty-reklamacje",
+    element: <Complaints />,
+  },
+  {
+    path: "/platnosc-udana",
+    element: <PaymentSuccess />,
+  },
+  {
+    path: "/platnosc-anulowana",
+    element: <PaymentCancel />,
+  },
+  { path: "*", element: <NotFound /> },
 ];
 
 export default routes;
