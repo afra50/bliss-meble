0854fbdd88bfdcb8ee8478ca897f186bf30ac979
Emilia <afra57@onet.pl>
2026-03-03 23:50:50 +0100

    kontakt szkic

---
+++ frontend/src/pages/Contact.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "../components/ui/Button";
+import "../styles/pages/contact.scss";
+
+function Contact() {}
+
+export default Contact;
+
+// 1. Dane kontaktowe, dane firmy
+// 2. Formularz kontaktowy - użyj klas dla inputow, labelek oraz div otaczających je oba zgodnie z forms.scss. Dla checkboxa z polityką też klasa z forms.scss
+// 3. mapka z pinezka - narazie mozesz uzyc jakies zdjecie losowe
+// 4. social media - ig i fb
+// 5. Nie musisz importować zadnych plikow scss
+// 6. strona pod adresem /kontakt
